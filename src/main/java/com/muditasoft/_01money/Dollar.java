c5fd4762eb2d2d57af8b32b21a2e4f8d0b6e9cd8
tutkuince <tutku.ince@outlook.com>
2020-02-27 15:41:55 +0300

    equality for all

--- src/main/java/com/muditasoft/_01money/Dollar.java
+++ src/main/java/com/muditasoft/_01money/Dollar.java
@@ -1,20 +1,8 @@
 package com.muditasoft._01money;
 
-import java.util.Objects;
-
-public class Dollar {
-
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
-        this.amount = amount;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
         this.amount = amount;
     }
 
@@ -22,16 +10,4 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Dollar)) return false;
-        Dollar dollar = (Dollar) o;
-        return getAmount() == dollar.getAmount();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getAmount());
-    }
 }
--- src/main/java/com/muditasoft/_01money/Franc.java
+++ src/main/java/com/muditasoft/_01money/Franc.java
@@ -1,19 +1,7 @@
 package com.muditasoft._01money;
 
-import java.util.Objects;
-
-public class Franc {
-    private int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
-        this.amount = amount;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
         this.amount = amount;
     }
 
@@ -21,16 +9,4 @@ public class Franc {
         return new Franc(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Franc)) return false;
-        Franc franc = (Franc) o;
-        return getAmount() == franc.getAmount();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getAmount());
-    }
 }
